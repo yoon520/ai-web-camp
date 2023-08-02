82404a19cf7a3bdd97a5f83229482fa227fd454e
yoon520 <gmlwjddbs123@naver.com>
2023-08-02 11:17:03 +0900

    문자열 비교 과정

---
+++ assignment/string_compare.py
@@ -0,0 +1,14 @@
+# 문자열 비교 샘플 코드
+def compare_str(str1, str2):
+    flag = True
+    if len(str1) == len(str2):
+        for i, j in zip(str1, str2):
+            if i.encode('utf-8') != j.encode('utf-8'):
+                flag = False
+                break
+    else:
+        flag = False
+    return flag
+
+print(compare_str("바나나", "바나나"))      # True
+print(compare_str("애플망고", "바나나"))    # False
